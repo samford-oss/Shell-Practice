06dcbfbe79027260acec21778d2de67fd448faf0
Shaik Saddam Ali <shaiksamford@gmail.com>
2025-06-10 23:01:38 +0530

    hello world started

--- 03.variables.sh
+++ 03.variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-PERSON1=Trump
-PERSON2=Musk
+PERSON1=trump
+PERSON2=musk
 
-echo "$PERSON1:: Hey PERSON2, How are you?"
-echo "$PERSON2:: Hello PERSON1, I am good. How are you doing?"
+echo "$PERSON1:: Hey $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am good. How are you doing?"
 echo "$PERSON1:: I am fine too. Whats up?"
 echo "$PERSON2:: nothing,just going to Mars now, will you come?"
 echo "$PERSON1:: Sorry, you carry on! I will come once you come back"
