3987a35f513b7c5a10f8fec0a22f037dcce4e7f9
Shaik Saddam Ali <shaiksamford@gmail.com>
2025-06-10 23:12:12 +0530

    hello world started

---
+++ 04.variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hey $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am good. How are you doing?"
+echo "$PERSON1:: I am fine too. Whats up?"
+echo "$PERSON2:: nothing,just going to Mars now, will you come?"
+echo "$PERSON1:: Sorry, you carry on! I will come once you come back"
