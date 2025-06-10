39203d03976a58a00e4dbd7e566ae15ab612742f
Shaik Saddam Ali <shaiksamford@gmail.com>
2025-06-10 23:28:02 +0530

    hello world started

---
+++ 05.variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Enter your Pin Number::"
+
+read -s PIN #here PIN is variable and -s we use to hide the pin that we entering
+
+echo "Your Number is: $PIN" #$PIN we are using to display the pin that we entered
