1041d59c271aa5372bc4a70b24006a6b399a6f12
Shaik Saddam Ali <shaiksamford@gmail.com>
2025-06-11 11:35:31 +0530

    hello world started

--- 07.arrays.sh
+++ 07.arrays.sh
@@ -4,5 +4,5 @@ MOVIES=("Court" "HIT3" "DEVARA" "PUSPA2") #TOTAL WE HAVE 4 VALUES STORED WITH PO
        #  0       1       2        3
 
 # NOW I WANT TO PRINT FIRST MOVIE
-echo "FRIST MOVIE: $MOVIES[0]"
+echo "FRIST MOVIE: ${MOVIES[0]}" # we are keeping flower braces means everything inside of it works as variable
 echo "here is the first movie name"
