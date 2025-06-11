4cd1e93ea02f2464dfeeac4fcf55506a9e6e9e17
Shaik Saddam Ali <shaiksamford@gmail.com>
2025-06-11 11:40:39 +0530

    hello world started

--- 07.arrays.sh
+++ 07.arrays.sh
@@ -6,3 +6,5 @@ MOVIES=("Court" "HIT3" "DEVARA" "PUSPA2") #TOTAL WE HAVE 4 VALUES STORED WITH PO
 # NOW I WANT TO PRINT FIRST MOVIE
 echo "FRIST MOVIE: ${MOVIES[0]}" # we are keeping flower braces means everything inside of it works as variable
 echo "Last MOVIE name: ${MOVIES[3]}"
+
+echo "All Movies list: ${MOVIES[@]}" #TO display all movies we are using @ symbol
