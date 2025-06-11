080f85b8d724a10505ca14c2e07f795b763a6cb0
Shaik Saddam Ali <shaiksamford@gmail.com>
2025-06-11 11:31:29 +0530

    hello world started

---
+++ 07.arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("Court" "HIT3" "DEVARA" "PUSPA2") #TOTAL WE HAVE 4 VALUES STORED WITH POSITIONS 0 1 2 3
+       #  0       1       2        3
+
+# NOW I WANT TO PRINT FIRST MOVIE
+echo "FRIST MOVIE: $MOVIES[0]"
+echo "here is the first movie name"
