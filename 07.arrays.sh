9b288bb26b7681e7c92bc175da4f2cb906499530
Shaik Saddam Ali <shaiksamford@gmail.com>
2025-06-11 11:47:34 +0530

    hello world started

--- 07.arrays.sh
+++ 07.arrays.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MOVIES=("Court" "HIT3" "DEVARA" "PUSPA2") #TOTAL WE HAVE 4 VALUES STORED WITH POSITIONS 0 1 2 3
-       #  0       1       2        3
+       #  0       1       2        3  positions
 
 # NOW I WANT TO PRINT FIRST MOVIE
 echo "FRIST MOVIE: ${MOVIES[0]}" # we are keeping flower braces means everything inside of it works as variable
