ae37e7fd11bfbf4c95b128345ab8f44255982495
Shaik Saddam Ali <shaiksamford@gmail.com>
2025-06-14 11:22:45 +0530

    special vars

--- 09.Conditions.sh
+++ 09.Conditions.sh
@@ -4,7 +4,7 @@ NUMBER=$!
 
 if [ $NUMBER -lt 10 ]
 then
-echo "Given Number $NUMBER is less than 10"
+    echo "Given Number $NUMBER is less than 10"
 else
-echo "Given Number $NUMBER is not less than 10"
+    echo "Given Number $NUMBER is not less than 10"
 fi
